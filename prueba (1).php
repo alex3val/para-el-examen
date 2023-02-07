9aa0713cae7f79ee513058afe18b999184f36af1
alex3val <122468684+alex3val@users.noreply.github.com>
2023-02-07 17:32:08 +0100

    Add files via upload

---
+++ prueba (1).php
@@ -0,0 +1,12 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+//  $comments = $_POST['comments'];
+  $gender = $_POST['gender'];
+
+ echo "datos:" . "<br>";
+ echo "nombre: ", $name . "<br>";
+ echo "email: ", $email . "<br>";
+ echo "genero: ", $gender . "<br>";
+
+?>
